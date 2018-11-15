7cfaade57722086985af81c6defaa9c4e893872e
venkat-yarlagadda <venkat.yarlagadda87@gmail.com>
2018-11-16 02:34:03 +0530

    Update UserManagement.java

--- UserManagement.java
+++ UserManagement.java
@@ -3,4 +3,8 @@ public class UserManagement{
   public static void main(String[] args){
     System.out.println("First GIT usage file");
   }
+  
+  public void display(){
+    System.out.println("Welcome to display Users");
+  }
 }
